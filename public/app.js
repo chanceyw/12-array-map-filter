0361e364aaae149308be5d247f3d70774f60a7d8
Chancey Ward <chancey_ward@yahoo.com>
2015-11-08 14:23:30 -0600

    completed first question

--- public/app.js
+++ public/app.js
@@ -1,3 +1,11 @@
 (function() {
 
+  var priceSum = items.reduce((currentSum, item) => {
+    return currentSum + item.price;
+  }, 0);
+
+  var averagePrice = priceSum / items.length;
+
+  console.log(averagePrice);
+
 })();
