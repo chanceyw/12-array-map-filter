a57d3e1ac0e27d70fba2abaaf89f108d0d99bc4c
Chancey Ward <chancey_ward@yahoo.com>
2015-11-09 20:51:28 -0600

    question 3 fix

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 (function() {
-/* globals items */
+  /* globals items */
   var priceSum = items.reduce((currentSum, item) => {
     return currentSum + item.price;
   }, 0);
 
   var averagePrice = priceSum / items.length;
 
-  console.log(averagePrice); // function for question one ^
+  // console.log(averagePrice); // function for question one ^
 
   var inPrice = items.filter((item) => {
     return item.price > 14 && item.price < 18;
@@ -14,14 +14,15 @@
   .map(function(item) {
     return item.title;
   });
-  console.log(inPrice); // function for question two ^
+
+  // console.log(inPrice); // function for question two ^
 
   var curr = items.filter((item) => {
-   return item.currencyCode === ‘GBP’;
+    return item.currencyCode === 'GBP';
   })
-  .map(function(item) {
-    return item title + ‘cost’ + item.price;
-  });
-  console.log(curr); // function for question three ^
+ .map(function(item) {
+  return item.title + 'cost' + item.price;
+});
 
+  console.log(curr); // function for question three ^
 })();
