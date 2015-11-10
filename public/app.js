d4478b8f169eeb733a3b15c278a05003ca869dca
Chancey Ward <chancey_ward@yahoo.com>
2015-11-09 19:35:37 -0600

    question 2

--- public/app.js
+++ public/app.js
@@ -6,6 +6,15 @@
 
   var averagePrice = priceSum / items.length;
 
-  console.log(averagePrice);
+  console.log(averagePrice); // function for question one ^
+
+  var inPrice = items.filter((item) => {
+    return item.price > 14 && item.price < 18;
+    map(function(item) {
+      return items.title;
+      console.log(inPrice);
+    });
+  });
+
 
 })();
